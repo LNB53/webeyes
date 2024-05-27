fb04c5c5b5b0ab36d5b305326ed5f675a8f036cc
Quinten Van der Wildt <r0937889@student.thomasmore.be>
2024-05-27 12:43:06 +0200

    aanmaken van LOGIN API endpoint en js

---
+++ resources/html/js/login.js
@@ -0,0 +1,27 @@
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch('http://localhost:8080/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ mail: email, password: password })
+        });
+
+        if (response.ok) {
+            const userData = await response.json();
+            // Store user data in browser
+            localStorage.setItem('user', JSON.stringify(userData.user));
+            window.location.href = "dashboard.html"; // Redirect to dashboard
+        } else {
+            const errorData = await response.json();
+            alert(`Error: ${errorData.detail}`);
+        }
+    } catch (error) {
+        alert(`Error: ${error.message}`);
+    }
+});
