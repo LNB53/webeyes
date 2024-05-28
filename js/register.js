a1178daf9c39115d97e128a70c9f47ba32fe7e9e
Quinten Van der Wildt <r0937889@student.thomasmore.be>
2024-05-28 13:52:17 +0200

    inloggen met hashed passwoorden serversided gelukt extra test

--- js/register.js
+++ js/register.js
@@ -14,7 +14,7 @@ document.getElementById('registerForm').addEventListener('submit', async functio
         return;
     }
 
-    const response = await fetch('http://localhost:8080/register', {
+    const response = await fetch('http://127.0.0.1:8080/register', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
