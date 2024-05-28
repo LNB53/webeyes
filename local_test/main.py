58f5142a51ac56f42d9aae4b631652037b4c1815
Quinten Van der Wildt <r0937889@student.thomasmore.be>
2024-05-28 15:49:22 +0200

    toevoeging van authentication aan de pagina's waar inlog vereist voor is

--- local_test/main.py
+++ local_test/main.py
@@ -33,7 +33,7 @@ class User(BaseModel):
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host="10.0.0.243",
+            host="localhost",
             user="administrator",
             password="administrator",
             database="login_database"
