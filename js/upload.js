d13e797e04497337bb236252cca0a58e810d0daa
www-data <r0930134@student.thomasmore.be>
2024-05-28 18:23:37 +0000

    push from ubuntu

--- js/upload.js
+++ js/upload.js
@@ -22,3 +22,43 @@ function removeFile() {
     fileNameDisplay.textContent = '';
     removeFileBtn.classList.add('hidden');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const success = urlParams.get('s');
+    if (success === 'T') {
+        document.getElementById('upload-success-message').classList.remove('hidden');
+        setTimeout(function() {
+            document.getElementById('upload-success-message').classList.add('hidden');
+            
+            // Remove success parameter from URL
+            const newParams = new URLSearchParams(window.location.search);
+            newParams.delete('s');
+            const newParamsString = newParams.toString();
+            const newUrl = window.location.pathname + (newParamsString ? '?' + newParamsString : '');
+            window.history.replaceState({}, '', newUrl);
+        }, 2500);
+    }
+});
+
+function validateFileType() {
+    const fileInput = document.getElementById('dropzone-file');
+    const fileName = fileInput.value;
+    const allowedExtensions = /(\.rar|\.zip)$/i;
+
+    if (!allowedExtensions.exec(fileName)) {
+        alert('Invalid file type. Only .rar and .zip files are allowed.');
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// Function to show spinner when form is submitted
+function showSpinner() {
+    document.getElementById('upload-spinner').classList.remove('hidden');
+    return true; // Allow form submission
+}
+
+// Add event listener to form submission
+document.getElementById('upload-form').addEventListener('submit', showSpinner);
