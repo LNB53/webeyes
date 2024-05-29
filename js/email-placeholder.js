beafa1cf07f2aae925a7a9dc79865513e12c5a31
Quinten Van der Wildt <r0937889@student.thomasmore.be>
2024-05-29 11:13:56 +0200

    aanvoeging van password change knop bij api

--- js/email-placeholder.js
+++ js/email-placeholder.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Function to decode JWT token
+    function decodeJwtToken(token) {
+        const payloadBase64 = token.split('.')[1];
+        const payload = JSON.parse(atob(payloadBase64));
+        return payload;
+    }
+
     // Get the JWT token from localStorage
     const token = localStorage.getItem("accessToken");
 
     if (token) {
         try {
             // Decode the JWT token
-            const decodedToken = jwt_decode(token);
+            const decodedToken = decodeJwtToken(token);
 
             // Get the email from the decoded token
             const userEmail = decodedToken.mail;
