cffdddecad2d3933d28f6da33b7479faf3f6007c
Quinten Van der Wildt <r0937889@student.thomasmore.be>
2024-05-29 09:50:37 +0200

    aanvoeging van delete knop bij api

--- js/logout.js
+++ js/logout.js
@@ -1,7 +1,7 @@
-document.getElementById('logoutButton').addEventListener('click', function() {
+function logout() {
     // Remove items from localStorage
     localStorage.clear();
 
     // Redirect to login page
     window.location.href = "login.html";
-});
+}
